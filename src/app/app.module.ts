fdc312c44ac634cd888c2304334be79b9d1e42e0
codebr3ak <barakaralph@gmail.com>
2021-02-11 12:47:40 +0300

    Form component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
