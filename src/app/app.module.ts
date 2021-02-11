a05247d51822c4c9b0c83c7a0822ff9e94e7b86b
codebr3ak <barakaralph@gmail.com>
2021-02-11 23:41:40 +0300

    file modifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,37 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
-import { FormComponent } from './form/form.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { GitsearchComponent } from './gitsearch/gitsearch.component';
-import { ProfileComponent } from './profile/profile.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { GithubService } from './github.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
+// import { HomeComponent } from './home/home.component';
+// import { GitsearchComponent } from './gitsearch/gitsearch.component';
+// import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent,
-    FormComponent,
     HeaderComponent,
-    HomeComponent,
-    GitsearchComponent,
-    ProfileComponent,
     FooterComponent,
-    PageNotFoundComponent
+    routingComponents,
+    PageNotFoundComponent,
+    // HomeComponent,
+    // GitsearchComponent,
+    // ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+
   ],
-  providers: [],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/github.service.spec.ts
+++ src/app/github.service.spec.ts
@@ -5,10 +5,7 @@ import { GithubService } from './github.service';
 describe('GithubService', () => {
   let service: GithubService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GithubService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
